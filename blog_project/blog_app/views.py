43019a4896f4f6967c0ea91d7cebf34e149a9c8f
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2023-09-11 17:44:48 +0900

    add : 앱 views 기본틀 추가

--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def board_cilent(request):
+    return render(request, 'blog_app/board-cilent.html')
+def board_admin(request):
+    return render(request, 'blog_app/board-admin.html')
+def login(request):
+    return render(request, 'blog_app/login.html')
+def write(request):
+    return render(request, 'blog_app/write.html')
+def board(request):
+    return render(request, 'blog_app/board.html')
