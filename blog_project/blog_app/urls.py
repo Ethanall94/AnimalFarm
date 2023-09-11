712c762f8a8a4db463035325efafb89f54f42371
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2023-09-11 17:35:17 +0900

    add : app url 추가

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
+from . import views
+# from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    
+    path('', views.board_cilent, name='board-cilent'), # 메인 페이지
+    path('write', views.write, name='write'),
+    path('login', views.login, name='login'),
+    path('board_admin',views.board_admin, name='board_admin'),
+    path('board',views.board, name='board')
 ]
