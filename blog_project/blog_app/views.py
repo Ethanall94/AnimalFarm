76c2e2f66e800985fdf721dda558842a2156fb22
bnz18 <benuze18@gmail.com>
2023-09-12 15:42:09 +0900

    fix: views.py viewset 추가 및 경로 변
    views.py viewset 추가 및 urls.py 경로 변경

--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render, redirect
+from .models import boardPost
+from rest_framework import viewsets
+from .serializers import boardPostSerializer
 
 # Create your views here.
 
-def board_cilent(request):
-    return render(request, 'blog_app/board-cilent.html')
+def board_client(request):
+    return render(request, 'board-client.html')
 def board_admin(request):
-    return render(request, 'blog_app/board-admin.html')
+    return render(request, 'board-admin.html')
 def login(request):
-    return render(request, 'blog_app/login.html')
+    return render(request, 'login.html')
 def write(request):
-    return render(request, 'blog_app/write.html')
+    return render(request, 'write.html')
 def board(request):
-    return render(request, 'blog_app/board.html')
+    try:
+        post = boardPost.objects.latest('create_date')
+    except:
+        post = None
+    return render(request, 'board.html', {'post': post})
+
+
+class boardPostViewset(viewsets.ModelViewSet):
+    queryset = boardPost.objects.all()
+    serializer_class = boardPostSerializer
