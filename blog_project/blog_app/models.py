69704a1bc10409e5ba96338dc1608ecc70c5b08b
bnz18 <benuze18@gmail.com>
2023-09-12 15:43:17 +0900

    fix: models.py에 컬럼 5개 추가
    models.py에 컬럼 5개 추가하였습니다
    
    pgadmin에서 추가 되는 것 확인했습니다.

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class boardPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.CharField(max_length=50)
+    create_date = models.DateField(auto_now_add=True)
+    tag = models.CharField(max_length=50)
