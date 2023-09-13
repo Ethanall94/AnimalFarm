cd5aac20cf9ad1f98b131de393d047c64b88b2e4
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2023-09-13 22:06:45 +0900

    Fix : 글 작성 시 태그가 보이는 오류 수정

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -1,13 +1,18 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=10)
+    title = models.CharField(max_length=50)
     content = models.TextField()
-    author = models.CharField(max_length=50)
-    create_date = models.DateField(auto_now_add=True)
-    tag = models.CharField(max_length=50)
+    create_at = models.DateTimeField(default=timezone.now)
+    author_id = models.CharField(max_length=50)
+    topic = models.CharField(max_length=50)
+    views = models.PositiveIntegerField(default=0)
+    update_at = models.DateTimeField(default=timezone.now)
+
+    # image = models.ImageField(upload_to='images/')
 
     def __str__(self): 
         return self.title
--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .forms import PostForm
 from .models import Post
 from rest_framework import viewsets
@@ -11,8 +13,28 @@ def board_client(request):
     return render(request, 'board-client.html')
 def board_admin(request):
     return render(request, 'board-admin.html')
-def login(request):
-    return render(request, 'login.html')
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect('/')
+        else:
+            # Return an 'invalid login' error message.
+            messages.success(request, "Oops! Try Again...")
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Hey, you logged out! Please Try Again...")
+    return redirect('/')
 
 # @login_required  #로그인 시 작성할 수 있도록 설정(로그인 설정 후 활성화)
 def write(request):
@@ -26,18 +48,30 @@ def write(request):
         form = PostForm()
     return render(request, 'write.html', {'form' : form})
 
-def board(request):
-    posts = Post.objects.all().order_by('-create_date')
-    return render(request, 'board.html', {'posts': posts})
+def board(request, topic=None):
+    try:
+        if topic:
+            main_post = Post.objects.filter(topic=topic).order_by('-create_at').first()
+            recommended_posts = Post.objects.filter(topic=topic).exclude(id=main_post.id).order_by('-create_at')[:2]
+        else:
+            main_post = Post.objects.order_by('-create_at').first()
+            recommended_posts = Post.objects.exclude(id=main_post.id).order_by('-create_at')[:2]
+    except:
+        main_post = None
+        recommended_posts = None
+    context = {
+        'main_post': main_post,
+        'recommended_posts': recommended_posts,
+    }
+
+    return render(request, 'board.html', context)
+            
 
 # def board(request):
-#     try:
-#         post = Post.objects.latest('create_date')
-#     except:
-#         post = None
-#     return render(request, 'board.html', {'post': post})
+#     posts = Post.objects.all().order_by('-create_date')
+#     return render(request, 'board.html', {'posts': posts})
 
 
 class PostViewset(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
+    queryset = Post.objects.all().order_by('-views')
     serializer_class = PostSerializer
