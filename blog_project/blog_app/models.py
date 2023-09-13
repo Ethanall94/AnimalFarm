cc56721002d06aeecb8db8870ee52a8aaba6b529
bnz18 <benuze18@gmail.com>
2023-09-13 10:31:17 +0900

    fix: models.py 데이터 값 세팅 및 시간대변경
    models.py 데이터 값 세팅 및 시간대
    변경

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
@@ -5,9 +6,10 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=10)
     content = models.TextField()
-    author = models.CharField(max_length=50)
-    create_date = models.DateField(auto_now_add=True)
-    tag = models.CharField(max_length=50)
+    create_at = models.DateTimeField(default=timezone.now)
+    author_id = models.CharField(max_length=50)
+    topic = models.CharField(max_length=50)
+    views = models.PositiveIntegerField(default=0)
 
     def __str__(self): 
         return self.title
