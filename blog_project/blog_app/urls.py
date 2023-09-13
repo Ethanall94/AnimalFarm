a6cf2ff0309dfab0edfd3e66ff9ea8d41f93597c
bnz18 <benuze18@gmail.com>
2023-09-13 13:40:56 +0900

    Update urls.py

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -11,8 +11,8 @@ router.register(r'posts', PostViewset)
 urlpatterns = [
     path('', views.board_client, name='board-client'), # 메인 페이지
     path('write/', views.write, name='write'),
-    path('login', views.login_user, name='login'),
-    path('logout', views.logout_user, name='logout'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
     path('board_admin/',views.board_admin, name='board_admin'),
     path('board/',views.board, name='board'),
     path('board/<str:topic>/', views.board, name='board-topic'),
