ed3cc78ef5b93144e7089ca4d28bd583732d443b
Ethanall94 <Ethanall94@gmail.com>
2023-09-13 09:44:19 +0900

    feat: urls.py 수정

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('', views.board_client, name='board-client'), # 메인 페이지
     path('write/', views.write, name='write'),
     path('login', views.login, name='login'),
-    path('board_admin',views.board_admin, name='board_admin'),
+    path('board_admin/',views.board_admin, name='board_admin'),
     path('board/',views.board, name='board'),
     path('api/', include(router.urls)),
 ]
