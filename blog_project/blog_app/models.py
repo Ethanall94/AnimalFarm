34a5d6cee24b7fdff84e6f28b72d944e9c73d190
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2023-09-14 09:30:28 +0900

    add : contentposter img

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
     topic = models.CharField(max_length=50)
     views = models.PositiveIntegerField(default=0)
     update_at = models.DateTimeField(default=timezone.now)
+    content_poster = models.ImageField(null=True, upload_to="", blank=True)
 
     # image = models.ImageField(upload_to='images/')
 
